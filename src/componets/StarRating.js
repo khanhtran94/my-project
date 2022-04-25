0b230da3518f660260bbea5aba41b7a1a79366dc
khanhTran <khanhtrantung190894@gmail.com>
2022-04-25 23:32:59 +0700

    use useState

---
+++ src/componets/StarRating.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+export default function StarRating({ totalStars = 5 }) {
+  const Star = ({ selected = false }) => (
+    <FaStar color={selected ? "red" : "grey"} />
+  );
+  const createArray = length => [...Array(length)];
+  const [selectedStarts] = useState(3);
+
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => 
+        <Star key={i} selected={selectedStarts > i} />)}
+      <p>
+        {selectedStarts} of {totalStars} starts
+      </p>
+    </>
+
+  );
+
+}
