64a8d4064807f2471f74b1b178f130543a6f6d36
khanhTran <khanhtrantung190894@gmail.com>
2022-04-30 11:26:05 +0700

    add style, props for div container of component

--- src/componets/StarRating.js
+++ src/componets/StarRating.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-export default function StarRating({ totalStars = 5 }) {
+export default function StarRating({ style = {}, totalStars = 5, ...props}) {
   const [selectedStarts, setSelectedStars] = useState(0);
   const Star = ({ selected = false, onSelect = f => f }) => (
     <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
@@ -9,17 +9,16 @@ export default function StarRating({ totalStars = 5 }) {
   const createArray = length => [...Array(length)];
 
   return (
-    <>
-      {createArray(totalStars).map((n, i) => 
+    <div style={{ padding: "5px", ...style }} {...props}>
+      {createArray(totalStars).map((n, i) =>
         <Star key={i}
-         selected={selectedStarts > i}
-         onSelect={() => setSelectedStars(i+1)}
-         />)}
+          selected={selectedStarts > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />)}
       <p>
         {selectedStarts} of {totalStars} starts
       </p>
-    </>
+    </div>
 
   );
-
 }
