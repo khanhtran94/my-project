a15b9ff7f4c1cc2b1b49d1142fc5e94e6cc3d8da
khanhTran <khanhtrantung190894@gmail.com>
2022-04-30 22:24:22 +0700

    add form color uncontroller

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import './App.css';
 import React, { useState } from "react";
 import colorData from './data/color-data.json';
 import ColorList from './componets/ColorList';
-
+import AddColorForm from './componets/AddColorForm'
 function App() {
   const [colors, setColors] = useState(colorData)
   return (
-    <ColorList 
-    colors={colors}
-    onRemoveColor={id => {
-      const newColors = colors.filter(color => color.id !== id);
-      setColors(newColors);
-    }}
-    onRateColor = {(id, rating) => {
-      const newColors = colors.map(color => color.id === id ? {...color, rating} : color)
-      setColors(newColors);
-    }}
-     />
+    <>
+      <AddColorForm
+        onNewColor={(title, color) => alert(`TODO: Create ${title} - ${color}`)}
+      />
+      <ColorList
+        colors={colors}
+        onRemoveColor={id => {
+          const newColors = colors.filter(color => color.id !== id);
+          setColors(newColors);
+        }}
+        onRateColor={(id, rating) => {
+          const newColors = colors.map(color => color.id === id ? { ...color, rating } : color)
+          setColors(newColors);
+        }}
+      />
+    </>
+
   );
 }
 
