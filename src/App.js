0d02b201f0b4cfc0e327a7c73908baef7fb596ff
khanhTran <khanhtrantung190894@gmail.com>
2022-05-01 15:56:28 +0700

    del code in app

--- src/App.js
+++ src/App.js
@@ -6,35 +6,10 @@ import AddColorForm from './componets/AddColorForm'
 import { v4 } from "uuid";
 
 function App() {
-  const [colors, setColors] = useState(colorData)
   return (
     <>
-      <AddColorForm
-        onNewColor={(title, color) => {
-          const newColors = [
-            ...colors,
-              {
-                id: v4(),
-                rating: 0,
-                title: title,
-                color: color
-              }
-            
-          ];
-          setColors(color);
-        }}
-      />
-      <ColorList
-        colors={colors}
-        onRemoveColor={id => {
-          const newColors = colors.filter(color => color.id !== id);
-          setColors(newColors);
-        }}
-        onRateColor={(id, rating) => {
-          const newColors = colors.map(color => color.id === id ? { ...color, rating } : color)
-          setColors(newColors);
-        }}
-      />
+      <AddColorForm />
+      <ColorList />
     </>
 
   );
