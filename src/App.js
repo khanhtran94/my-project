93e1dad5dcfdbcb052e0ff2c99690cd5da3e72a8
khanhTran <khanhtrantung190894@gmail.com>
2022-05-03 16:33:53 +0700

    useEffect every type

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Checkbox from './componets/Checkbox';
 
 function App() {
+  const [val, set] = useState("");
+  const [phrase, setPharse] = useState("init phase");
+
+  const createPhrase = () => {
+    set("");
+    setPharse(val);
+  }
+
+  useEffect(() => {
+  console.log(`typep "${val}"`)
+  });
+
+  useEffect(() => {
+    console.log(`saved pharse "${phrase}"`)
+  });
+
   return (
     <React.StrictMode>
-      <Checkbox />
+     <label>Favorite phrase:</label>
+      <input
+        value={val}
+        placeholder={phrase}
+        onChange={e => set(e.target.value)}
+      />
+      <button onClick={createPhrase}>send</button>
     </React.StrictMode>
 
   );
