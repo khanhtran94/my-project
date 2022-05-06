7a37cda660f6f1d265c9c490441f3cba0cfc2f1d
khanhTran <khanhtrantung190894@gmail.com>
2022-05-07 00:53:24 +0700

    commom error when managing state is to overwite the state, lost name, info different

--- src/App.js
+++ src/App.js
@@ -1,95 +1,38 @@
-import './App.css';
-import React, { useEffect, useLayoutEffect, useMemo, useReducer, useState } from "react";
+import React, { useState } from "react";
 
+const firstUser = {
+  id: "0391-3233-3201",
+  firstName: "Bill",
+  lastName: "Wilson",
+  city: "Missoula",
+  state: "Montana",
+  email: "bwilson@mtnwilsons.com",
+  admin: false
+};
 
-const useAnyKeyToRender = () => {
-  const [, forceRender] = useState();
+function User() {
+  const [user, setUser] = useState(firstUser);
 
-  useEffect(()=>{
-    window.addEventListener("keydown", forceRender);
-    return () => window.removeEventListener("keydown", forceRender);
-
-  }, []);
-}
-
-
-function WordCount({children=""}) {
-  useAnyKeyToRender();
-  const words = useMemo(() => {
-    const works = children.split(" ");
-    return works;
-  },[children]);
-
-  useEffect(() => {
-    console.log("fress render");
-  }, [words]);
-
-  
   return (
-    <>
-      <p>{children}</p>
+    <div>
+      <h1>
+        {user.firstName} {user.lastName} - {user.admin ? "Admin" : "User"}
+      </h1>
+      <p>Email: {user.email}</p>
       <p>
-        <strong>{words.length} - words</strong>
+        Location: {user.city}, {user.state}
       </p>
-    </>
-  );
-  
-};
-function Checkbox(params) {
-  const [checked, toggle] = useReducer(checked => !checked, false);
- 
-
-  return(
-    <>
-      <input 
-
-        type={"checkbox"}
-        value={checked}
-        onChange={toggle}
-      />
-      {checked ? "checked" : "not checked"}
-    </>
+      <button
+        onClick={() => {
+          setUser({ admin: true });
+        }}
+      >
+        Make Admin
+      </button>
+    </div>
   );
 }
-function useWindowSize() {
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
 
-  const resize = () => {
-    setWidth(window.innerWidth);
-    setHeight(window.innerHeight);
-  };
-
-  useLayoutEffect(() => {
-    window.addEventListener("resize", resize);
-    resize();
-    return () => window.removeEventListener("resize", resize);
-  }, []);
-
-  return [width, height];
-};
-function useMousePosition() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-
-  const setPosition = ({ x, y }) => {
-    setX(x);
-    setY(y);
-  };
-
-  useLayoutEffect(() => {
-    window.addEventListener("mousemove", setPosition);
-    return () => window.removeEventListener("mousemove", setPosition);
-  }, []);
-
-  return [x, y];
-};
-function App() {
-
-  return (
-    <Checkbox />
-  );
-
+export default function App() {
+  return <User />;
 }
-
-export default App;
