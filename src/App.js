3401a1d436927e89ef028731681a00526afb5d96
khanhTran <khanhtrantung190894@gmail.com>
2022-05-06 23:09:33 +0700

    component word count

--- src/App.js
+++ src/App.js
@@ -11,20 +11,30 @@ const useAnyKeyToRender = () => {
 
   }, []);
 }
-const word = ["gnar", "123"];
 
 
-function App() {
+function WordCount({children=""}) {
   useAnyKeyToRender();
-
+  const words = children.split("");
 
   useEffect(() => {
-    console.log("fresh render");
-  }, [word]);
-  return (
-    <h1>Open the console</h1>
+    console.log("fress render");
+  }, [words]);
 
+  return (
+    <>
+      <p>{children}</p>
+      <p>
+        <strong>{words.length} - words</strong>
+      </p>
+    </>
   );
+  
+}
+function App() {
+
+  return <WordCount>You are not going to believe this but...</WordCount>;
+
 }
 
 export default App;
