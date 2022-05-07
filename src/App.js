b5354c4d703aefa5a4fd08ee3409e0e421de44cb
khanhTran <khanhtrantung190894@gmail.com>
2022-05-08 00:11:55 +0700

    bug not render

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import React from "react";
+import faker from "faker";
+
+const bigList = [...Array(100)].map(() => ({
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  avatar: faker.internet.avatar()
+}));
 
 const tahoe_peaks = [
   { name: "Freel Peak", elevation: 10891 },
@@ -18,20 +25,33 @@ function List({ data = [], renderItem, renderEmpty }) {
   );
 }
 
+function UserComponent({ data = [] }) {
+  console.log(data);
+  return (
+    <ul>
+      <li>
+      {data.map((item, i) => {
+        <p>{data.length}</p>
+      })}
+      </li>
+      <p>{data.length}</p>
+    </ul>
+  );
+}
+
 export default function App() {
+  const renderItem = item => (
+    <div style={{ display: "flex" }}>
+      <img src={item.avatar} alt={item.name} width={50} />
+      <p>
+        {item.name} - {item.email}
+      </p>
+    </div>
+  );
+
   return (
     <>
-      <List
-        data={tahoe_peaks}
-        renderEmpty={<p>This list is empty</p>}
-
-        renderItem={item => (
-          <>
-            {item.name} - {item.elevation.toLocaleString()}ft
-          </>
-        )}
-      />
-
+      <UserComponent data={bigList} />
     </>
   );
 }
