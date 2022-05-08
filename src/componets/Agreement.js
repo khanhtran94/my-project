557f0737215a29aaf1e646e299da031e77d789f5
khanhTran <khanhtrantung190894@gmail.com>
2022-05-08 23:04:58 +0700

    add agreement compoent

---
+++ src/componets/Agreement.js
@@ -0,0 +1,9 @@
+export default Agreement({onAgree = f=> f}) {
+  return(
+    <div>
+      <p>Terms...</p>
+      <p>These are the terms and stuff. Do you agree?</p>
+      <button onClick={onAgree}>I agree</button>
+    </div>
+  );
+}
