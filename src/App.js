669bdb2e5a4cfb6ba5ef5b2104e9d7089e3751aa
khanhTran <khanhtrantung190894@gmail.com>
2022-05-08 23:24:46 +0700

    lazy load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
-import Main from "./componets/Main";
+import React, { useState, lazy, Suspense } from "react";
 import Agreement from "./componets/Agreement";
-
 const BreakThings = () => {
   throw new Error("We intentionally broke something");
 };
+const Main = lazy(() => import("./componets/Main"));
 
 export default function App() {
   const [agree, setAgree] = useState(false);
@@ -13,6 +12,10 @@ export default function App() {
     return <Agreement onAgree={() => setAgree(true)} />
   }
   return (
-    <Main />
+    <Suspense>
+      <Main />
+
+    </Suspense>
+
   );
 }
