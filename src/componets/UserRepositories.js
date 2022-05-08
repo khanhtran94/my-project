9c77db94201403a67c54cfb8487752e0899239e9
khanhTran <khanhtrantung190894@gmail.com>
2022-05-08 16:30:00 +0700

    add login for repo menu;

--- src/componets/UserRepositories.js
+++ src/componets/UserRepositories.js
@@ -11,6 +11,7 @@ export default function UserRepositories({
       uri={`https://api.github.com/users/${login}/repos`}
       renderSuccess={({ data }) => (
         <RepoMenu
+          login={login}
           repositories={data}
           selectedRepo={selectedRepo}
           onSelect={onSelect}
