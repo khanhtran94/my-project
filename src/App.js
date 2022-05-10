b1a148e0227ce20ddb9f123ba05014d3a301c48e
khanhTran <khanhtrantung190894@gmail.com>
2022-05-10 22:49:34 +0700

    handle 3 state of Fetch componet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { useState, lazy, Suspense } from "react";
 import Agreement from "./componets/Agreement";
+import ErrorBoundary from "./componets/ErrorBoundary";
 const BreakThings = () => {
   throw new Error("We intentionally broke something");
 };
 const Main = lazy(() => import("./componets/Main"));
+// const loadStatus = () => "success - ready";
+// const loadStatus = () => {
+//   throw new Error("test");
+// };
 
+const loadStatus = () => {
+  throw new Promise(resolve => null);
+};
+
+function Status() {
+  const status = loadStatus();
+
+  return <h1>Status {status}</h1>
+}
 export default function App() {
   const [agree, setAgree] = useState(false);
 
@@ -12,10 +26,10 @@ export default function App() {
     return <Agreement onAgree={() => setAgree(true)} />
   }
   return (
-    <Suspense>
-      <Main />
-
+    <Suspense fallback={<GridLoader />}>
+      <ErrorBoundary>
+        <Status />
+      </ErrorBoundary>
     </Suspense>
-
   );
 }
