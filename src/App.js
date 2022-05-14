71d03599718799f82d449169e19df7bd86cd6589
khanhTran <khanhtrantung190894@gmail.com>
2022-05-14 10:45:27 +0700

    not sub router

--- src/App.js
+++ src/App.js
@@ -3,30 +3,39 @@ import { GridLoader } from "react-spinners";
 import Agreement from "./componets/Agreement";
 import ErrorBoundary from "./componets/ErrorBoundary";
 import Pages from "./componets/Pages";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import {
   Home,
   About,
   Events,
   Products,
   Contact,
-  Whoops404
+  Whoops404,
+  Services,
+  History,
+  Location,
 } from './componets/Pages'
 export default function App() {
-  
+
 
   return (
     <Suspense fallback={<GridLoader />}>
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<About />}>
+
+
+            <Route from="/history" to="/about/history" />
+            <Route from="/services" to="/about/services" />
+            <Route from="/location" to="/about/location" />
+          </Route>
           <Route path="/events" element={<Events />} />
 
           <Route path="/products" element={<Products />} />
 
           <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Whoops404 />}/>
+          <Route path="*" element={<Whoops404 />} />
         </Routes>
       </div>
     </Suspense>
--- src/componets/Pages.js
+++ src/componets/Pages.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import {
+  Link,
+  useLocation,
+  Outlet
+} from "react-router-dom";
 import '../stylesheets/pages.scss'
 
 export function Home() {
@@ -21,6 +25,7 @@ export function About() {
   return (
     <div>
       <h1>[About]</h1>
+      <Outlet />
     </div>
   );
 }
@@ -29,7 +34,7 @@ export function About() {
 export function Events() {
   return (
     <div>
-      <section className="events">
+      <section>
         <h1>[Event Calendar]</h1>
       </section>    </div>
   );
@@ -59,4 +64,63 @@ export function Whoops404() {
       <h1>Reource 404 {location.pathname}</h1>
     </div>
   );
+}
+
+export function Services() {
+  <section>
+    <h2>Our Services</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Integer nec odio. Praesent
+      libero. Sed cursus ante dapibus diam. Sed
+      nisi. Nulla quis sem at nibh elementum
+      imperdiet. Duis sagittis ipsum. Praesent
+      mauris. Fusce nec tellus sed augue semper
+      porta. Mauris massa. Vestibulum lacinia arcu
+      eget nulla. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per
+      inceptos himenaeos. Curabitur sodales ligula
+      in libero.
+    </p>
+  </section>;
+}
+
+
+export function History() {
+  <section>
+    <h2>Our Services</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Integer nec odio. Praesent
+      libero. Sed cursus ante dapibus diam. Sed
+      nisi. Nulla quis sem at nibh elementum
+      imperdiet. Duis sagittis ipsum. Praesent
+      mauris. Fusce nec tellus sed augue semper
+      porta. Mauris massa. Vestibulum lacinia arcu
+      eget nulla. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per
+      inceptos himenaeos. Curabitur sodales ligula
+      in libero.
+    </p>
+  </section>;
+}
+
+
+export function Location() {
+  <section>
+    <h2>Our Services</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Integer nec odio. Praesent
+      libero. Sed cursus ante dapibus diam. Sed
+      nisi. Nulla quis sem at nibh elementum
+      imperdiet. Duis sagittis ipsum. Praesent
+      mauris. Fusce nec tellus sed augue semper
+      porta. Mauris massa. Vestibulum lacinia arcu
+      eget nulla. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per
+      inceptos himenaeos. Curabitur sodales ligula
+      in libero.
+    </p>
+  </section>;
 }
